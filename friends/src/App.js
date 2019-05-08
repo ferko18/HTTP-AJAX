b484f9c5a80ae927c6ff118280440c784b9a7458
Firealem Erko <firealemg@yahoo.com>
2019-05-08 15:56:38 -0400

    setup get request, data fetched

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
-import './App.css';
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      test 
-    </div>
-  );
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(res => {
+        this.setState({ friends: res.data });
+      })
+      .catch(err => {
+        this.setState({ error: err.response.message });
+      });
+  }
+
+  render() {
+    return <div className="App">test</div>;
+  }
 }
 
 export default App;
