755d32f744d8c65d51fc3cf950d94312ec441f4b
Firealem Erko <firealemg@yahoo.com>
2019-05-09 15:26:42 -0400

    Friends day 2 initial commit

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,35 +1,38 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "./App.css";
-import Friends from './Components/Friends'
-import Form from './Components/Form'
+import Friends from "./Components/Friends";
+import Form from "./Components/Form";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      friends: []
+      friends: [],
+      
     };
   }
-
+ 
   componentDidMount() {
     axios
       .get("http://localhost:5000/friends")
       .then(res => {
-        this.setState({ friends: res.data });
+        this.setState({ friends: res.data }); console.log(res)
+        
       })
       .catch(err => {
         this.setState({ error: err.response.message });
       });
-
-      
-  }
+  };
 
   render() {
-    return <div className="App">
-    <Form />
-    <Friends friends ={this.state.friends}/>
-    </div>;
+    
+    return (
+      <div className="App">
+        <Form />
+      <Friends friends={this.state.friends} />
+      </div>
+    );
   }
 }
 
--- friends/src/Components/Form.js
+++ friends/src/Components/Form.js
@@ -1,34 +1,33 @@
-import React from 'react';
-import './form.css'
+import React from "react";
+import "./form.css";
 
 const Form = () => {
-    return (
-        <div>
-            <form class="contact">
-    <div class="outer">
-      <div class="label-text">Id</div>
-      <div class="input-text">
-        <input type="text" />
-      </div>
-      <div class="label-text">Name</div>
-      <div class="input-text">
-        <input type="text" />
-      </div>
-      <div class="label-text">Age</div>
-      <div class="input-text">
-        <input type="text" />
-      </div>
-      <div class="label-text">Email Address</div>
-      <div class="input-text">
-        <input type="email" />
-      </div>
-      
-      <div class="submitbtn">Save</div>
-      </div>
-      
-  </form>
+  return (
+    <div>
+      <form className="contact">
+        <div className="outer">
+          <div className="label-text">Id</div>
+          <div className="input-text">
+            <input type="text" />
+          </div>
+          <div className="label-text">Name</div>
+          <div className="input-text">
+            <input type="text" />
+          </div>
+          <div className="label-text">Age</div>
+          <div className="input-text">
+            <input type="text" />
+          </div>
+          <div className="label-text">Email Address</div>
+          <div className="input-text">
+            <input type="email" />
+          </div>
+
+          <div className="submitbtn">Save</div>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default Form;
