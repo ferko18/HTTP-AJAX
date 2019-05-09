f561c15699e1825eaf814621ea94754244cc8b0c
Firealem Erko <firealemg@yahoo.com>
2019-05-09 15:44:21 -0400

    converted form input to controlled input

--- friends/src/Components/Form.js
+++ friends/src/Components/Form.js
@@ -1,33 +1,43 @@
 import React from "react";
 import "./form.css";
 
-const Form = () => {
-  return (
-    <div>
-      <form className="contact">
-        <div className="outer">
-          <div className="label-text">Id</div>
-          <div className="input-text">
-            <input type="text" />
-          </div>
-          <div className="label-text">Name</div>
-          <div className="input-text">
-            <input type="text" />
-          </div>
-          <div className="label-text">Age</div>
-          <div className="input-text">
-            <input type="text" />
-          </div>
-          <div className="label-text">Email Address</div>
-          <div className="input-text">
-            <input type="email" />
-          </div>
+class Form extends React.Component {
+  state = {
+    friend: {
+      id: "",
+      name: "",
+      age: "",
+      email: ""
+    }
+  };
+  render() {
+    return (
+      <div>
+        <form className="contact">
+          <div className="outer">
+            <div className="label-text">Id</div>
+            <div className="input-text">
+              <input type="text" name="id" onChange={this.changeHandler} value={this.state.friend.id} />
+            </div>
+            <div className="label-text">Name</div>
+            <div className="input-text">
+              <input type="text" name="name" onChange={this.changeHandler} value ={this.state.friend.name}/>
+            </div>
+            <div className="label-text">Age</div>
+            <div className="input-text">
+              <input type="text" name="age" onChange={this.changeHandler} value ={this.state.friend.age}/>
+            </div>
+            <div className="label-text">Email Address</div>
+            <div className="input-text">
+              <input type="email" name="email" onChange={this.changeHandler} value ={this.state.friend.email} />
+            </div>
 
-          <div className="submitbtn">Save</div>
-        </div>
-      </form>
-    </div>
-  );
-};
+            <div className="submitbtn">Save</div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Form;
